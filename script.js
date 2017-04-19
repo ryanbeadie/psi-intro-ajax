f4c374fa3ce5834ec38f29c25ee252ccdc6a53bf
Ryan Beadie <ryansteelebeadie@yahoo.com>
2017-04-18 20:43:31 -0500

    need to add poster img

--- script.js
+++ script.js
@@ -10,15 +10,25 @@ function onReady() {
   // when response comes back from the server
   // run success function which will log response
   $.ajax({
-    url: 'http://www.omdbapi.com/?t=jaws',
+    url: 'http://www.omdbapi.com/?t=serpico',
     success: function(response) {
       console.log('this is the response ->', response);
-      console.log('movie title ->', response.Title);
+      console.log('movie title ->', response.Poster);
       // console.log('movie actors ->', response.Actors);
       // put title on the DOM
-      $('#movie-holder').append('<div class="movie-title">' + response.Title + '</div>');
+
+      //trying to append poster img to DOM
+      //$('#movie-poster').prepend('<img id="theImg" src="theImg.png" />')
+
+      $('#movie-poster').append('<div class="movie-poster">'  + response.Poster + '</div>');
+      $('#movie-holder').append('<div class="movie-title">' + 'Title: ' + response.Title + '</div>');
+      $('#movie-holder').append('<div class="movie-actors">' + 'Actors: ' + response.Actors + '</div>');
+      $('#movie-holder').append('<div class="movie-director">' + 'Director: ' + response.Director + '</div>');
+      $('#movie-holder').append('<div class="movie-awards">' + 'Awards: ' + response.Awards + '</div>');
+      //want to make the website a hyperlink
+      $('#movie-holder').append('<div class="movie-website">' + 'Website: ' +  response.Website + '</div>');
     }
-    // error: function(response) {
+    // error: function(response)
     //   console.log('error case: response ->', response);
     // }
   });
